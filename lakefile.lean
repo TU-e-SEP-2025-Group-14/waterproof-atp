cb6782169686cb6b9dca099dd89d3beee597856b
Serhiy Medvedyev <s.v.medvedyev@student.tue.nl>
2026-01-05 21:49:28 +0100

    add lean test file

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL
 
-require «WaterproofGenre» from git "https://github.com/TU-e-SEP-2025-Group-14/waterproof-genre.git"@"d463f5b"
+require «WaterproofGenre» from git "https://github.com/TU-e-SEP-2025-Group-14/waterproof-genre.git"@"e0e4b0418b7eba7a3c3f00dece5acca214c48ca0"
 require «verbose» from git "https://github.com/PatrickMassot/verbose-lean4.git"@"c978de7"
 
 package "WaterproofDocument" where
